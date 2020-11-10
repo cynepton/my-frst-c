f42a742ee38735bc0f3ae8d7ad22f6660dd27239
Isaac Aderonmu <aderonmuisaac@gmail.com>
2020-11-10 15:51:44 +0100

    feat: basic data types

---
+++ basics/dataTypes/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    // C datatypes
+
+    // Integer types
+    int integerType = 40;
+    
+    // Decimal types
+    // Double
+    double doubleType = 3.7;
+    // Float: Usually not used, just stick to doubles for now
+    float floatType = 3.72;
+
+    // Charactertypes
+    char charType = 'A';
+    // String of characters.
+    char charStringType[] = "This is a strig of characters";
+
+    return 0
+}
