c77316a053c11465e78877410d001f6640544f9f
Isaac Aderonmu <aderonmuisaac@gmail.com>
2020-11-10 16:00:39 +0100

    feat: add more print types

--- basics/helloWorld/main.c
+++ basics/helloWorld/main.c
@@ -14,10 +14,16 @@ int main () {
     // Integers
     int integerAge = 30;
 
+    // double
+    double doubleType = 3.7;
+
+
     // Use %s for strings and %d for integers
+    // The % and letter are called format specifiers
     printf("The character name is %s\n", characterName);
     printf("The character age is %d\n", integerAge);
-
+    printf("The character age is %f\n", doubleType);
+    printf("The character age is %f and the name is %s\n", doubleType, characterName);
     
     return 0;
 }
